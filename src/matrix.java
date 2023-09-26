3acb357e69f25b8e62dd1ad2703568985067adf4
Rafly Hanggaraksa <ajarafly422@gmail.com>
2023-09-26 21:01:20 +0700

    fixed minor bugs

--- src/matrix.java
+++ src/matrix.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.lang.Math;
 
 import javax.swing.plaf.BorderUIResource.MatteBorderUIResource;
-public class Matrix{
+public class Matrix {
     double [][] matrix;
     int row;
     int col;
@@ -52,7 +52,7 @@ public class Matrix{
     }
 
 
-    public void createMatrix(int row, int col) {
+    public void setMatrix(int nrow, int ncol) {
         // Memberikan matriks nilai baris dan kolom
         setRow(nrow);
         setCol(ncol);
@@ -70,7 +70,7 @@ public class Matrix{
     public Matrix add(Matrix m){
         // Menjumlahkan matrik dengan elemennya sendiri 
         Matrix result = new Matrix(); 
-        result.createMatrix(row, col);
+        result.setMatrix(row, col);
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 result.setMatrixValue(i, j, matrix[i][j] + m.matrix[i][j]);
@@ -84,7 +84,7 @@ public class Matrix{
 
         Matrix hasil = new Matrix();
         double sum=0;
-        hasil.createMatrix(a.getRow(), b.getCol());
+        hasil.setMatrix(a.getRow(), b.getCol());
         for(int i=0;i<a.getRow();i++){
             for(int j=0;j<b.getCol();j++){
                 sum=0;
@@ -107,10 +107,10 @@ public class Matrix{
     public static Matrix copyMatrix(Matrix m){
         // Memberi salinan dari matriks m
         Matrix result = new Matrix(); 
-        result.createMatrix(row, col);
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                result.setMatrixValue(i, j, matrix[i][j]);
+        result.setMatrix(m.row, m.col);
+        for(int i = 0; i < m.row; i++){
+            for(int j = 0; j < m.col; j++){
+                result.setMatrixValue(i, j, m.matrix[i][j]);
             }
         }
         return result;
@@ -120,10 +120,10 @@ public class Matrix{
     public static Matrix transpose(Matrix m){
         // Transpose matriks
         Matrix result = new Matrix(); 
-        result.createMatrix(row, col);        
-        for(int i = 0; i < col; i++){
-            for(int j = 0; j < row; j++){
-                result.setMatrixValue(i, j, matrix[j][i]);
+        result.setMatrix(m.row, m.col);        
+        for(int i = 0; i < m.col; i++){
+            for(int j = 0; j < m.row; j++){
+                result.setMatrixValue(i, j, m.matrix[j][i]);
             }
         }
         return result;
@@ -157,7 +157,7 @@ public class Matrix{
     public Matrix identity() {
         // Mengembalikan nilai matriks identitas NxN
         Matrix identitas = new Matrix();
-        identitas.createMatrix(this.row, this.col);
+        identitas.setMatrix(this.row, this.col);
         for(int i = 0; i < this.row; i++) {
             for (int j = 0; j < this.col; j++) {
                 if (i == j) {
@@ -180,7 +180,7 @@ public class Matrix{
         return true;
     }
     
-    public Matrix Gauss(Matrix m){
+    public static Matrix gauss(Matrix m){
         Matrix result = copyMatrix(m);
         for(int i = 0;i<result.row;i++){
             int pivotCol = result.swapZeroToBottom(i);
@@ -238,16 +238,7 @@ public class Matrix{
         }
     }
 
-    /* CHECK STATE */
-    public boolean isRow0(Matrix m,int row){
-        // Mengecek apakah nilai dari suatu baris bernilai 0
-        for(int i = 0;i<m.col-1;i++){
-            if(m.matrix[row][i]!=0){
-                return false;
-            }
-        }
-        return true;
-    }
+
 
     /* BACA ATAU TULIS MATRIKS */
     public void printMatriks(){
@@ -260,13 +251,13 @@ public class Matrix{
         }
     }
 
-    public void bacaMatriks () {
+    public void bacaMatriks() {
         Scanner sc = new Scanner(System.in);
         String dump;
         System.out.println("Enter row and col: ");
         int row = sc.nextInt();
         int col = sc.nextInt();
-        createMatrix(row, col);
+        setMatrix(row, col);
         System.out.println("Enter matrix: ");
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
@@ -276,68 +267,6 @@ public class Matrix{
         }
     }
     
-    public void bacaFileMatrix(String parlokasi,boolean parbic){
-        int row=0,col=0;
-        double dumpd;
-        String lokasi,dumps;
-        boolean bic;
-        Scanner sc= new Scanner(System.in);
-
-        lokasi=parlokasi;
-        bic=parbic;
-        if(lokasi==""){
-            System.out.printf("Masukkan nama file: ");
-            lokasi=sc.nextLine();
-        }
-        if(bic) row--;
-        try{
-            /*mengambil file */
-            File inp=new File(lokasi);
-            Scanner scf = new Scanner(inp);
-
-            /*mengambil 1 baris dan menghitung kolom */
-            Scanner cntcol = new Scanner(scf.nextLine());
-            row++;
-            while(cntcol.hasNextDouble()){
-                col++;
-                dumpd=cntcol.nextDouble();
-            }
-
-            /*menghitung baris */
-            while(scf.hasNextLine()){
-                dumps=scf.nextLine();
-                row++;
-            }
-        }
-        catch(Exception ex) {
-            ex.printStackTrace();
-        }
-        if(bic) createMatrix(16, 1);
-        else createMatrix(row, col);
-        //System.out.println(row+" "+col);
-        try{
-            File inp = new File(lokasi);
-            Scanner scf= new Scanner(inp);
-            int cnt=0;
-            for(int i=0;i<row;i++){
-                Scanner srow = new Scanner (scf.nextLine());
-                for(int j=0;j<col;j++){
-                    if(bic) this.matrix[cnt][0] = srow.nextDouble();
-                    else this.matrix[i][j] = srow.nextDouble();
-                }
-            }
-            if(bic){
-                Scanner srow = new Scanner (scf.nextLine());
-                this.tx = srow.nextDouble();
-                this.ty = srow.nextDouble();
-            }
-        }
-            
-        catch(Exception ex){
-            ex.printStackTrace();
-        }
-    }
-
      
     /* MAIN OPERATION */
     public Matrix inverseMatrix(Matrix par) {
@@ -346,7 +275,7 @@ public class Matrix{
         m = copyMatrix(par);
         Matrix identMatrix;
         identMatrix = new Matrix();
-        identMatrix.createMatrix(m.row, m.col);
+        identMatrix.setMatrix(m.row, m.col);
         identMatrix = m.identity();
         for (int n =0;n<this.row;n++) {
             double pivot = m.matrix[n][n];
@@ -394,37 +323,12 @@ public class Matrix{
     }
     
 
-    // Read or Write Matrix
-    public void printMatriks(){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                System.out.printf("%.2f ",matrix[i][j]); // Print dua angka dibelakang desimal
-            }
-            System.out.println();
-        }
-    }
 
-    public void bacaMatriks () {
-        Scanner sc = new Scanner(System.in);
-        String dump;
-        System.out.println("Enter row and col: ");
-        int nrow = sc.nextInt();
-        int ncol = sc.nextInt();
-        setMatrix(nrow, ncol);
-        System.out.println("Enter matrix: ");
-        for(int i = 0; i < nrow; i++){
-            for(int j = 0; j < ncol; j++){
-                setMatrixValue(i, j, sc.nextDouble());
-            }
-            dump=sc.nextLine();
-        }
-        // sc.close();
-    }
 
 
     public Matrix matrixBicubicSpline(){
         Matrix aBic = new Matrix();
-        aBic.createMatrix (16, 16);
+        aBic.setMatrix (16, 16);
         int cnt;
         cnt=0;
         for(int i=0;i<=1;i++){
@@ -487,51 +391,29 @@ public class Matrix{
                         System.out.printf("Pivot Modified: %f\n",pivot);
                         if (pivot != 0) {
                             break;
-            double determinant = 1;
-            Matrix result = new Matrix();
-            result = copyMatrix(this);
-            for (int n=0;n<this.row;n++) {
-                for (int i = n + 1; i<this.row;i++) {
-                    double pivot = result.matrix[n][n];
-                    int newN = i;
-    
-                    if (pivot == 0) { // Cek dan tuker bila nilai pivotnya nol
-                        while ((newN < row)) {
-                            swaprow(result, newN, n);
-                            pivot = result.matrix[n][n];
-                            if (pivot != 0) {
-                                break;
-                            }
-                            newN += 1;
-
                         }
                     }
-                    double scale = result.matrix[i][n] / pivot; 
-                    for (int j=0;j<this.col;j++) {
-                        result.matrix[i][j] -= result.matrix[n][j] * scale;
-                    }
                 }
-            } 
-    
-            for (int i=0;i<this.row;i++) { // Mengalikan determinan secara diagonal
-                determinant *= result.matrix[i][i];
+                double scale = result.matrix[i][n] / pivot; 
+                for (int j=0;j<this.col;j++) {
+                    result.matrix[i][j] -= result.matrix[n][j] * scale;
+                }
             }
-            if (determinant == 0) {
-                determinant = 0;
-            }
-            return determinant;
         } 
-        catch(ArrayIndexOutOfBoundsException n) {
-            System.out.println("error");
-            return 0;
+        for (int i=0;i<this.row;i++) { // Mengalikan determinan secara diagonal
+            determinant *= result.matrix[i][i];
         }
+        if (determinant == 0) {
+            determinant = 0;
+        }
+        return determinant;
     } 
 
     public double determinantCofacMatrix(){
         // Mencari determinant matriks menggunakan kofaktor
         
         return 0.0;
-
+    }
 
     public void bacaFileMatrix(String parlokasi,boolean parbic){
         int nrow=0,ncol=0;
@@ -621,7 +503,7 @@ public class Matrix{
         n = banyakTitik - 1;
 
         Matrix tabelMatrix = new Matrix();
-        tabelMatrix.createMatrix(banyakTitik,banyakTitik+1);
+        tabelMatrix.setMatrix(banyakTitik,banyakTitik+1);
         tabelMatrix.printMatriks();
         for(int i=0;i<banyakTitik;i++){
             System.out.println("Titik X:");
@@ -657,7 +539,7 @@ public class Matrix{
         a = new Matrix();
         b = new Matrix();
         a.bacaMatriks();
-        b.createMatrix(a.row, 1);
+        b.setMatrix(a.row, 1);
         Scanner sc = new Scanner(System.in);
         for (int i=0;i<a.row;i++){
             b.matrix[i][0] = sc.nextDouble();
