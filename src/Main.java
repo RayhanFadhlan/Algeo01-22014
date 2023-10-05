876ec5c0fd36b2bdd388995b127fead30ffc1ba4
RayNoor0 <rayendraalthaf04@gmail.com>
2023-10-05 21:23:56 +0700

    little fix on output

--- src/Main.java
+++ src/Main.java
@@ -184,8 +184,8 @@ public class Main {
 
         // hitung hasil
         hasil = yBic.bicMeasure(aBic);
-        sAns= String.format("(%f,%f) = ",yBic.tx,yBic.ty);
-        sAns = String.format("%.3f",hasil)+"\n";
+        sAns = String.format("(%f,%f) = ",yBic.tx,yBic.ty);
+        sAns += String.format("%.3f",hasil)+"\n";
 
         twrite = Matrix.isToFile();
         if(twrite){
